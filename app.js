afdff6dcfa5745eb956b51326d0e33177e5807fe
WebDev Journey <angeliesioson@gmail.com>
2020-10-02 12:02:13 +0800

    updated files

--- app.js
+++ app.js
@@ -11,16 +11,17 @@ searchUser.addEventListener('keyup', (e) => {
   // Get input text
   const userText = e.target.value;
 
-  if(userText !== '') {
+  if(userText !== ''){
    // Make http call
    github.getUser(userText)
     .then(data => {
       if(data.profile.message === 'Not Found') {
         // Show alert
-        // ui.showAlert('User not found', 'alert alert-danger');
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
         // Show profile
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     })
   } else {
